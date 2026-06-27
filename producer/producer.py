7946f2fd95fe714f3b34ff2a71bf2f9f7aaf77dd
s-u-priya <devisupriya6811@gmail.com>
2026-06-27 21:40:35 +0530

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -3,13 +3,11 @@ import pandas as pd
 import json
 import time
 
-
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
     value_serializer=lambda x:
     json.dumps(x).encode('utf-8')
 )
-
 
 df = pd.read_csv("data/paysim.csv")
 
