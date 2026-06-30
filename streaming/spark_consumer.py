65c275b8ad567f0208dc90ca9100bd07f7b99881
s-u-priya <devisupriya6811@gmail.com>
2026-06-30 21:05:05 +0530

    Add files via upload

---
+++ streaming/spark_consumer.py
@@ -0,0 +1,56 @@
+from pyspark.sql import SparkSession
+
+
+spark = (
+    SparkSession
+    .builder
+    .appName(
+        "FraudDetection"
+    )
+    .getOrCreate()
+)
+
+
+df = (
+
+    spark
+
+    .readStream
+
+    .format("kafka")
+
+    .option(
+        "kafka.bootstrap.servers",
+        "localhost:9092"
+    )
+
+    .option(
+        "subscribe",
+        "transactions"
+    )
+
+    .load()
+
+)
+
+
+query = (
+
+    df
+
+    .writeStream
+
+    .format(
+        "console"
+    )
+
+    .outputMode(
+        "append"
+    )
+
+    .start()
+
+)
+
+
+query.awaitTermination()
